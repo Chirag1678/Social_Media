d81ebdcad553acbe182de1804dc4c524c3f45da2
Chirag <chirag1678.be21@chitkara.edu.in>
2024-12-19 00:55:49 +0530

    minor changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const isProduction = mode === 'production';
+  const isDevelopment = mode === 'development';
 
   return {
     server: {
       proxy: {
-        '/api': isProduction 
-                  ? 'https://social-media-c7t8.onrender.com' 
-                  : 'http://localhost:8000'
+        '/api': isDevelopment
+                  ? 'http://localhost:8000'
+                  : 'https://social-media-c7t8.onrender.com' 
       }
     },
     plugins: [react()],
